771b984306da4627ae1308e01515754a08e0ed4c
bluestar-12 <svrushali50@gmail.com>
2024-11-27 22:11:31 +0530

    Update DAA_1_2.py

--- DAA_1_2.py
+++ DAA_1_2.py
@@ -1,6 +1,40 @@
 import random
 
 
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2 
+        left_half = arr[:mid] 
+        right_half = arr[mid:]  
+
+       
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        
+        i = j = k = 0  
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+       
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+
 def binary_search(arr, target):
     low = 0
     high = len(arr) - 1
@@ -16,16 +50,15 @@ def binary_search(arr, target):
 
 
 def main():
+   
+    arr = random.sample(range(1, 10001), 5000)
     
-    arr = random.sample(range(1, 1001), 500)
-    arr.sort() 
-    print("Generated Sorted Array:")
-    print(arr)
-    
-    
+    print("\nSorting the array using Merge Sort...")
+    merge_sort(arr)  
+   
     target = int(input("\nEnter the element to search: "))
     
-    # Perform binary search
+    
     result = binary_search(arr, target)
     print(result)
 
