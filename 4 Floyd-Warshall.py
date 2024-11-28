7412920887e162e3df1c369cbfc95df59758a164
bluestar-12 <svrushali50@gmail.com>
2024-11-28 12:03:27 +0530

    Update 4 Floyd-Warshall.py

--- 4 Floyd-Warshall.py
+++ 4 Floyd-Warshall.py
@@ -1,32 +1,28 @@
-
 def floyd_warshall(graph):
-   
     num_vertices = len(graph)
 
-   
     dist = [[graph[i][j] for j in range(num_vertices)] for i in range(num_vertices)]
 
-    
     for k in range(num_vertices):
         for i in range(num_vertices):
             for j in range(num_vertices):
-                
                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
     return dist
 
+num_vertices = int(input("Enter the number of vertices: "))
 
-graph = [
-    [0, 3, 999, 7],
-    [3, 0, 2, 1],
-    [999, 2, 0, 4],
-    [7, 1, 4, 0]
-]
+print("Enter the adjacency matrix (use a large number like 999 for infinity):")
+graph = []
+
+for i in range(num_vertices):
+    row = list(map(int, input(f"Enter row {i + 1} (space-separated): ").split()))
+    graph.append(row)
 
 
 shortest_paths = floyd_warshall(graph)
 
 
-print("Shortest path matrix:")
+print("\nShortest path matrix:")
 for row in shortest_paths:
-    print(row)
+    print(" ".join(map(str, row)))
